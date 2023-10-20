4f0a17958ca66234ed3289d909c01d8a45df9213
CafeManager <kevinflores1997@yahoo.com>
2023-10-19 23:45:20 -0700

    removed extra code added structure

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+const Card = ({ random, suit, value }) => {
+    const style = {
+        position: "absolute",
+        borderRadius: "3px",
+        transform: "rotate",
+    };
+
+    return <div> Post </div>;
+};
+
+export default Card;
---
+++ src/Deck.js
@@ -0,0 +1,9 @@
+import Card from './Card'
+
+const Deck = () => {
+    return (
+        <Card> </Card>
+    )
+}
+
+export default Deck 
